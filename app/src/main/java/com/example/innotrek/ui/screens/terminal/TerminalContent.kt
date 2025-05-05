ee52f65d0b4f072b1a71fdb5d6588339b146cef3
Hani <haniel.sor.3301@gmail.com>
2025-05-04 21:49:38 -0600

    Terminales

--- app/src/main/java/com/example/innotrek/ui/screens/terminal/TerminalContent.kt
+++ app/src/main/java/com/example/innotrek/ui/screens/terminal/TerminalContent.kt
@@ -4,6 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
-fun TerminalContent(navController: NavController){
+fun TerminalContent(){
     BarTerminal()
 }
